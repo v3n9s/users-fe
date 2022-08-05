cb6f9b07fdf9b106f08c69bd583176dfda4e840b
Venyamin Shvets <venyashvets@gmail.com>
2022-08-05 13:38:48 +0300

    feat: reset token if it's not valid

--- src/services/interceptors.ts
+++ src/services/interceptors.ts
@@ -1,11 +1,15 @@
 import { AxiosError, AxiosInstance } from 'axios';
 import { store } from '../store';
 import { showAlert } from '../store/alertsSlice';
+import { setUserToken } from '../store/userSlice';
 
 export const addInterceptors = (axiosInstance: AxiosInstance) => {
   axiosInstance.interceptors.response.use((res) => res, (error: AxiosError) => {
     if ((<any>error.response?.data)?.message) {
       store.dispatch(showAlert({ text: (<any>error.response!.data).message, variant: 'danger' }))
+    }
+    if (error.response?.status === 401) {
+      store.dispatch(setUserToken(''));
     }
     return error.response;
   });
