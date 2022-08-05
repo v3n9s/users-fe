ca15ff643e943f25c06cd82e30e5716831308dce
Venyamin Shvets <venyashvets@gmail.com>
2022-08-05 13:05:31 +0300

    feat: change creation way of axios instance

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,14 +1,14 @@
-import { axiosInstance } from './axios-instance';
+import { getAxiosInstance } from './axios-instance';
 
 export const registerUser = async (
   user: { name: string, email: string, password: string }
 ) => {
-  const res = await axiosInstance.post('/register', user);
+  const res = await getAxiosInstance().post('/register', user);
   return res.status === 201;
 }
 
 export const loginUser = async (user: { name: string, password: string }) => {
-  const res = await axiosInstance.post('/login', user);
+  const res = await getAxiosInstance().post('/login', user);
   if (res.status === 201) return <string>res.data.token;
   return false;
 }
--- src/services/axios-instance.ts
+++ src/services/axios-instance.ts
@@ -3,13 +3,16 @@ import config from '../config';
 import { store } from '../store';
 import { addInterceptors } from './interceptors';
 
-const token = store.getState().user.token;
+export const getAxiosInstance = () => {
+  const token = store.getState().user.token;
 
-export const axiosInstance = axios.create({
-  baseURL: config.BACKEND_URL,
-  headers: {
-    authorization: `Bearer ${token}`
-  }
-});
+  const axiosInstance = axios.create({
+    baseURL: config.BACKEND_URL,
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  });
 
-addInterceptors(axiosInstance);
+  addInterceptors(axiosInstance);
+  return axiosInstance;
+}
